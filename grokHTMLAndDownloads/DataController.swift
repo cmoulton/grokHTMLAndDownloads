b3c9466c9e7e1131b762fb07155ec3ec8ce70401
Christina Moulton <christina@teakmobile.com>
2015-10-12 13:10:38 -0300

    Added function to fetch charts, doesn’t provide them to anything yet though

--- grokHTMLAndDownloads/DataController.swift
+++ grokHTMLAndDownloads/DataController.swift
@@ -20,6 +20,53 @@ class DataController {
   }
   
   func fetchCharts() {
+    Alamofire.request(.GET, URLString)
+      .responseString { responseString in
+        if let htmlAsString = responseString.result.value {
+          // TODO checks from Alamofire page
+          let doc = HTMLDocument(string: htmlAsString)
+          if let tableContents = doc.firstNodeMatchingSelector("tbody") {
+            self.charts = []
+            for row in tableContents.children {
+              var url: String?
+              var number: Int?
+              var scale: Int?
+              var title: String?
+              if let rowElement = row as? HTMLElement { // TODO: should be able to combine this with loop above
+                // first column: URL and number
+                if let firstColumn = rowElement.children[0] as? HTMLElement {
+                  // skip the first row, or any other where the first row doesn't contain a number
+                  if let entry = firstColumn.childAtIndex(0) as? HTMLElement {
+                    url = entry.objectForKeyedSubscript("href") as? String
+                    if let contents = entry.childAtIndex(0) as? HTMLTextNode {
+                      number = Int(contents.textContent)
+                    }
+                  }
+                }
+                // second column: Scale
+                if (url == nil) {
+                  continue // can't do anything without a URL, e.g., the header row
+                }
+                if let secondColumn = rowElement.children[1] as? HTMLElement {
+                  if let entry = secondColumn.childAtIndex(0) as? HTMLTextNode {
+                    scale = Int(entry.textContent)
+                  }
+                }
+                // third column: Name
+                if let thirdColumn = rowElement.children[2] as? HTMLElement {
+                  if let entry = thirdColumn.childAtIndex(0) as? HTMLTextNode {
+                    title = entry.textContent
+                  }
+                }
 
+                if let title = title, url = url, number = number, scale = scale {
+                  let newChart = Chart(title: title, url: url, number: number, scale: scale)
+                  self.charts?.append(newChart)
+                }
+              }
+            }
+          }
+        }
+    }
   }
 }
